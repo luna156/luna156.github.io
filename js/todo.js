2275f3986acd3be314d235720fb011841a9f857e
luna156 <63947262+luna156@users.noreply.github.com>
2022-09-10 14:48:34 +0900

    list delete ok

--- js/todo.js
+++ js/todo.js
@@ -3,43 +3,56 @@ const todoInput = todoForm.querySelector("input");
 const todoList=document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
-const todos=[];
+let todos=[];
 
 function saveTodos(){
     localStorage.setItem("todos",JSON.stringify(todos));
 }
 
+
+function filter(item){
+    return item !==3;
+}
+
+
 function deleteTodo(event){
     const li = event.target.parentElement;
+    console.log(typeof(li.id));
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
     li.remove();
 }
 
 
-function paintTodo(newTodo){
-
+function paintTodo(newTodoObj){
     const li = document.createElement("li");
+    li.id = newTodoObj.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click",deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText =newTodo;
+    span.innerText = newTodoObj.text;
     todoList.appendChild(li);
-     
 }
 
 function handleTodoSubmut(event){
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    todos.push(newTodo);
-    paintTodo(newTodo);
+
+    const newTodoObj ={
+        text: newTodo,
+        id: Date.now()
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodos();
 }
 
 
-
+ 
 
 
 todoForm.addEventListener("submit",handleTodoSubmut);
@@ -48,5 +61,5 @@ const savedTodos = localStorage.getItem(TODOS_KEY);
 
 if(savedTodos){
     const parsedTodos =  JSON.parse(savedTodos);
-    parsedTodos.forEach((item)=>paintTodo(item));
+    parsedTodos.forEach(paintTodo);
 }
---
+++ js/weather.js
@@ -0,0 +1,10 @@
+function onGeoOk(){
+
+}
+
+function onGeoError(){
+    alert("Can't find location");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
